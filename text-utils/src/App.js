2edd56730b2865fa90a5f198ca3e02e846352524
Jahnvi Gangwar <59444913+jahnvigangwar@users.noreply.github.com>
2023-05-30 00:22:41 +0530

    Update App.js

--- text-utils/src/App.js
+++ text-utils/src/App.js
@@ -1,13 +1,33 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-
+import TextForm from './components/TextForm';
+import React, {useState} from 'react'
 
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const toggleMode = () =>{
+    if(mode === 'light')
+    {
+      setMode('dark');
+      document.body.style.backgroundColor = 'grey';
+    }
+    else
+    {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
     {/* <Navbar title="textUtills" aboutText="About Us"/>; */}
-    <Navbar title="textUtills" />;
+    <Navbar title="textUtills" mode = {mode} toggleMode = {toggleMode} />;
+    <div className="container my-3">
+   <TextForm heading="Enter your text here"  mode = {mode} />
+    </div>
+    {/* <About/> */}
+    
     </>
   );
 }
