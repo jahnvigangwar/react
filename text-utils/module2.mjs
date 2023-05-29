67dfc8f861097486aaa38ff66b6383387011b75a
Jahnvi Gangwar <59444913+jahnvigangwar@users.noreply.github.com>
2023-05-29 06:34:13 +0530

    Add files via upload

---
+++ text-utils/module2.mjs
@@ -0,0 +1,9 @@
+const a = "ABC";
+const b = "BCD";
+const c = "CDE";
+const d = "DEF";
+
+export default c;
+export {a};
+export {c};
+export {d};
---
+++ text-utils/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './components/Navbar';
+
+
+
+function App() {
+  return (
+    <>
+    {/* <Navbar title="textUtills" aboutText="About Us"/>; */}
+    <Navbar title="textUtills" />;
+    </>
+  );
+}
+
+export default App;
